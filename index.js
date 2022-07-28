83d47fb2562cfcdbb7c189bfca4fae21ca3bd484
Cassandra Lee <cassichan@Cassandras-MacBook-Air.local>
2022-07-27 21:37:11 -0400

    removing restaurant via delete

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const data = require("./restaraunts.json");
-const fs = require("fs"); //File system so we can manipulate JSON file and store it again
+const fs = require("fs");
+//File system so we can manipulate JSON file and store it again
 
 const app = express();
-
 const PORT = 4000;
 
 app.use(cors());
@@ -21,18 +21,18 @@ const handleWriteFile = () => {
 app.get("/all-restaraunts", (req, res) => res.send(data));
 
 //add new restaraunt
-app.post("/add-restaraunt", (req, res) => {
+app.post("/add-restaraunts", (req, res) => {
   // console.log('this is body', req.body)
   data.push(req.body); //add new restaraunt to array
-  handleWriteFile();
+  handleWriteFile(); //call function to convert back to JSON
   res.send(data); //Send all restaraunts
 });
 
 //find and update a restaraunt
 app.patch("/update-restaraunt", (req, res) => {
   //find file to update
+  const { name } = req.query; //Using name in query params
   console.log(req.query);
-  const { name } = req.query;
   console.log("This is: ", name);
   const itemFound = data.find((eachRestaraunt) => eachRestaraunt.name === name);
   const indexOfItem = data.indexOf(itemFound); //getting index of item found
@@ -45,3 +45,18 @@ app.patch("/update-restaraunt", (req, res) => {
 });
 
 //find and delete a restaraunt
+app.delete("/delete-restaraunts", (req, res) => {
+  //find restaraunt to delete
+  const { name } = req.query;
+  console.log("This is: ", name);
+  const itemFound = data.find((eachRestaraunt) => eachRestaraunt.name === name);
+  const indexOfItem = data.indexOf(itemFound);
+  console.log(
+    "This is the index of the restaraunt we want to remove: ",
+    indexOfItem
+  );
+  //remove restaraunt using the index
+  data.splice(14, 1);
+  handleWriteFile();
+  res.send(data);
+});
